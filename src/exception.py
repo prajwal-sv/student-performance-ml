3331fab4e5021ef55a5d0af87cb5ed151438c503
Prajawal <prajwalwankhede890@gmail.com>
2026-03-28 22:30:32 +0530

    chore: clean up code structure and remove unused code blocks

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-from logger import logger   # ✅ import your logger
-
+from src.logger import logging
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     
--- src/logger.py
+++ src/logger.py
@@ -4,21 +4,13 @@ from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-# Create logs directory
-log_dir = os.path.join(os.getcwd(), "logs")
-print("Log directory:", log_dir)
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
 
-os.makedirs(log_dir, exist_ok=True)
-
-LOG_FILE_PATH = os.path.join(log_dir, LOG_FILE)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
-logger = logging.getLogger(__name__)
-
-# Force log creation
-logger.info("Logging started successfully")
