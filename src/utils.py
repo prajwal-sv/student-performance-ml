1c47ccc2b90bf75bd6ced8ddfeb8b61485a18718
Prajawal <prajwalwankhede890@gmail.com>
2026-03-29 16:13:05 +0530

    feat: implement data transformation and preprocessing pipelines

---
+++ src/utils.py
@@ -0,0 +1,63 @@
+import os
+import sys
+
+import numpy as np 
+import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model_name = list(models.keys())[i]
+
+            para = param[model_name]
+
+            # GridSearchCV with parameters
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            # Best model
+            best_model = gs.best_estimator_
+
+            # Predictions
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
+
+            # Scores
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try :
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+    
+    except Exception as e :
+        raise CustomException(e,sys)
